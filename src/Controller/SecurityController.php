6edd74bfb19d866f36ed906f786943fba12be034
Ludivine <mrt.ludivine@gmail.com>
2024-01-10 09:56:55 +0100

    login dashboard admin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,20 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use App\Form\LoginType;
 
-class SecurityController extends AbstractController {
-
-    #[Route(path: '/connexion', name: 'app_login_index')]
-    public function login(AuthenticationUtils $authenticationUtils)
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $form = $this->createForm(LoginType::class);
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-    
-        return $this->render('login/index.html.twig', [
-            'form' => $form->createView(),
-            'error' => $error,
-        ]);
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
